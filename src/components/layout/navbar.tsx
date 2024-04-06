7e1ace73959bfbe5916584c0c6b657e448d4290d
Karthik Mallya N <kars13m@gmail.com>
2024-04-06 19:23:15 +0530

    Refactor styles in globals.css, remove unused code in confirm.tsx and card.module.css, update button styles in button.tsx, and delete unused files city_shadow.svg and card.tsx

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -8,13 +8,13 @@ import AuthButton from "../auth/authButton";
 export function Navbar() {
   return (
     <>
-      <nav className="sticky inset-x-0 top-0 z-30 h-16 w-full  border-gray-200  backdrop-blur-lg transition-all dark:bg-black">
+      <nav className="sticky inset-x-0 top-0 z-30 h-16 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all dark:bg-black">
         <MaxWidthWrapper>
-          <div className="flex h-16  rounded-full px-2 my-3 items-center justify-between  bg-gray-400  bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-gray-100">
+          <div className="flex h-16 items-center justify-between border-b border-zinc-200">
             <Link href="/" className="z-40 flex font-semibold">
-              <div className="flex h-14 items-center justify-between gap-3">
+              <div className="flex h-14 items-center justify-between gap-2">
                 <Image src="/logo.png" alt="logo" width={50} height={50} />
-                <span className="text-[#5b6d42] font-bold text-xl">WebWay.</span>
+                <span className="text-[#5b6d42] ">WebWay.</span>
               </div>
             </Link>
             <div className="hidden items-center space-x-4 sm:flex">
--- src/pages/standard.tsx
+++ src/pages/standard.tsx
@@ -1,15 +1,14 @@
 "use client";
 import React from "react";
-import { Label } from "../../components/ui2/label";
-import { Input } from "../../components/ui2/input";
-import { cn } from "../../lib/utils";
+import { Label } from "../components/ui2/label";
+import { Input } from "../components/ui2/input";
+import { cn } from "../lib/utils";
 
 import {
   IconBrandGithub,
   IconBrandGoogle,
   IconBrandOnlyfans,
 } from "@tabler/icons-react";
-import Link from "next/link";
 
 export default function SignupFormDemo() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -19,7 +18,7 @@ export default function SignupFormDemo() {
   const destinationOptions = ['Mangalore', 'Manipal', 'padubi', 'Udupi', 'Kundapura', 'Karkala', 'Moodbidri', 'Kinnigoli', 'Surathkal', 'Mulki', 'Bantwal', 'Puttur', 'Sullia', 'Belthangady', 'Dharmasthala', 'Karkala', 'Kapu', 'Kundapur', 'Karkal', 'Kota'];
   return (
     
-    <div className="max-w-md w-full my-6 mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input h-full  bg-gray-200  bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-80 border border-gray-100">
+    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Welcome to WebWay
       </h2>
@@ -44,8 +43,8 @@ export default function SignupFormDemo() {
           <Input id="phone" placeholder="Phone" type="number" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
-                <label htmlFor="destination" className="block text-sm font-medium  ">Destination</label>
-                <select id="destination" className="mt-1 block w-full pl-3 pr-10 py-2 text-base text-neutral-400 border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+                <label htmlFor="destination" className="block text-sm font-medium bg-gray-50 text-gray-700">Destination</label>
+                <select id="destination" className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
                     {/* Map over the destination options array */}
                     {destinationOptions.map((option, index) => (
                         <option key={index} value={option}>{option}</option>
@@ -80,13 +79,7 @@ export default function SignupFormDemo() {
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          <Link
-                href="/standard/confirm"
-               
-              >
-                Submit &rarr;
-              </Link>
-         
+          Submit &rarr;
           <BottomGradient />
         </button>
 
@@ -118,4 +111,3 @@ const LabelInputContainer = ({
     </div>
   );
 };
-
